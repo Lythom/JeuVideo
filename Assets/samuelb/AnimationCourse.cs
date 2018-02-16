eb45ae3bceb85f14118ce206e4b278494cf4a369
Samuel Bouchet <samuel.bouchet@gmail.com>
2018-02-16 15:53:58 +0100

    Add Move and animations

---
+++ Assets/samuelb/AnimationCourse.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[System.Serializable]
+public struct Animation {
+    public string name;
+
+    public Sprite[] sprites;
+}
+public class AnimationCourse : MonoBehaviour {
+
+    public Animation[] anims;
+    public int imagesParSecondes = 24;
+
+    private SpriteRenderer spriteRenderer;
+
+    private int currentSpriteIdx = 0;
+    private int currentAnim = 0;
+    private float accumulateur = 0;
+
+    // Use this for initialization
+    void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer> ();
+    }
+
+    public void SetSpeed (float v) {
+        this.spriteRenderer.flipX = v < 0 || this.spriteRenderer.flipX && v == 0;
+        if (Mathf.Abs (v) > 0 && currentAnim != GetAnimIndex ("Run")) {
+            ChangeAnimation ("Run");
+        }
+        if (Mathf.Abs (v) == 0 && currentAnim != GetAnimIndex ("Iddle")) {
+            ChangeAnimation ("Iddle");
+        }
+    }
+
+    void ChangeAnimation (string anim) {
+        this.currentAnim = GetAnimIndex (anim);
+        this.currentSpriteIdx = 0;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // durée souhaitée d'une frame
+        float frameDuration = GetFrameDurationInSec ();
+
+        // durée accumulée depuis le dernier changement de frame
+        accumulateur += Time.deltaTime;
+
+        // vide l'accumulateur et fait avancer les frames        
+        if (accumulateur > frameDuration) {
+            NextFrame ();
+            accumulateur -= frameDuration;
+        }
+
+    }
+
+    int GetAnimIndex (string animName) {
+        return System.Array
+            .FindIndex (anims, anim => {
+                return anim.name == animName;
+            });
+    }
+
+    float GetFrameDurationInSec () {
+        return 1f / imagesParSecondes;
+    }
+
+    public void NextFrame () {
+        currentSpriteIdx = (currentSpriteIdx + 1) % anims[currentAnim].sprites.Length;
+        spriteRenderer.sprite = anims[currentAnim].sprites[currentSpriteIdx];
+    }
+}
---
+++ Assets/samuelb/MoveLion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLion : MonoBehaviour {
+
+	public float speed = 1f; // unit per second
+
+	private AnimationCourse ac;
+
+	// Use this for initialization
+	void Start () {
+		ac = GetComponent<AnimationCourse>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		Vector3 pos = this.transform.position;
+		this.transform.position = new Vector3(
+			pos.x + horizontal * speed * Time.deltaTime,
+			pos.y + vertical * speed * Time.deltaTime,
+			pos.z
+		);
+
+		ac.SetSpeed(horizontal * speed * Time.deltaTime);
+	}
+}

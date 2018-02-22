4b2a7da0c6f2bc60349007caa7b2560620fa7bbd
AlexLP <alexlepoupon@gmail.com>
2018-02-22 10:55:49 +0100

    add first map

---
+++ Assets/AlexandreLePoupon/Script/AnimationCourse.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[System.Serializable]
+public struct Animation {
+    public string name;
+
+    public Sprite[] sprites;
+}
+public class AnimationCourse : MonoBehaviour {
+
+    // liste des animations configurables
+    public Animation[] anims;
+
+    // nombre d'images par seconde à laquelle l'animation doit être jouée
+    public int imagesParSecondes = 12;
+
+    // références interne
+    private SpriteRenderer spriteRenderer;
+
+    // état privé
+    // indice de la frame actuellement affichée
+    private int currentSpriteIdx = 0;
+    // indice de l'animation actuellement affichée    
+    private int currentAnim = 0;
+    // accumulateur pour mesurer le temps cumulé qui passe
+    private float accumulateur = 0;
+
+    // Use this for initialization
+    void Start () {
+        // Récupère une référence au script AnimationCourse attaché au même GameObject
+        spriteRenderer = GetComponent<SpriteRenderer> ();
+    }
+
+    public void SetAnimationFromSpeed (float vitesse) {
+        // Si la vitesse est négative, on va vers la gauche et le sprite devrait être inversé
+        // Si la vitesse est positive, on va vers la droite et le sprite ne devrait pas être inversé
+        // Si on allait précédemment vers la gauche (flipX est vrai) et que la vitesse tombe à 0, on reste vers la gauche
+        this.spriteRenderer.flipX = vitesse < 0 || this.spriteRenderer.flipX && vitesse == 0;
+        // Si le personnage se déplace et qu'il n'est pas déjà en train de courir, le faire courir
+        if (vitesse != 0 && currentAnim != GetAnimIndex ("Run")) {
+            ChangeAnimation ("Run");
+        }
+        // Si le personnage est arrêté et qu'il n'est pas déjà en animation d'attente, déclencher l'animation d'attente.       
+        if (vitesse == 0 && currentAnim != GetAnimIndex ("Iddle")) {
+            ChangeAnimation ("Iddle");
+        }
+    }
+
+    public void ChangeAnimation (string anim) {
+        this.currentAnim = GetAnimIndex (anim);
+        // Lors d'une nouvelle animation, repartir à la première image de cette animation
+        this.currentSpriteIdx = 0;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // durée souhaitée d'une frame
+        float frameDuration = GetFrameDurationInSec ();
+
+        // durée accumulée depuis le dernier changement de frame
+        accumulateur += Time.deltaTime;
+
+        // vide l'accumulateur et fait avancer les frames        
+        while (accumulateur > frameDuration && frameDuration > 0) {
+            NextFrame ();
+            accumulateur -= frameDuration;
+        }
+    }
+
+    private int GetAnimIndex (string animName) {
+        // cherche dans un tableau par condition sur le nom de l'animation
+        return System.Array.FindIndex (anims, anim => {
+                return anim.name == animName;
+            });
+    }
+
+    private float GetFrameDurationInSec () {
+        // 1 seconde divisée par le nombre de frame à afficher par secondes nous donne le temps
+        // qu'il convient d'allouer à chaque frame (en seconde).
+        return 1f / imagesParSecondes;
+    }
+
+    private void NextFrame () {
+        // boucle du début à la fin du tableau, revient au début lorsqu'on dépasse la fin
+        // grace à la fonction modulo (%).
+        currentSpriteIdx = (currentSpriteIdx + 1) % anims[currentAnim].sprites.Length;
+        // Affiche dans le sprite renderer la frame en cours de l'animation en cours.
+        spriteRenderer.sprite = anims[currentAnim].sprites[currentSpriteIdx];
+    }
+}
---
+++ Assets/AlexandreLePoupon/Script/Track.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Track : MonoBehaviour {
+
+	public Transform target;
+	public Vector2 offset;
+	public float trackingDistance = 1f;
+	public float stiffness = 0.05f;
+
+	// Update is called once per frame
+	void Update () {
+		if (target == null) return;
+
+		// Position vers laquelle la caméra doit tendre
+		// attention à bien conserver la position Z actuelle
+		// on ne poursuit que les positions x et y
+		Vector3 targetPos = new Vector3 (
+			target.position.x + offset.x,
+			target.position.y + offset.y
+		);
+
+		// l'offset est un déplacement relatif 
+		// On va appliquer à la position de la caméra
+		// Il est initialisé à zéro (pas de mouvement)
+		Vector3 moveOffset = Vector3.zero;
+
+		// Si la cible s'éoigne de la distance de Tracking
+		// Le offset va prendre une valeur pour déplacer la Caméra vers la cible
+		// Attention, ici on utiliser Vector2.Distance plutôt que Vector3.Distance, 
+		//		      car seule la distance des coordonnées x et y nous intéresse.
+		//            Avec Vector3.Distance la distance en Z serait prise en compte aussi
+		//            ce qui n'est pas souhaité.
+		//            Les Vector3 passés en paramètre sont transformé automatique en Vector2.
+		if (Vector2.Distance (target.position, this.transform.position) > trackingDistance) {
+			// C'est la formule magique du easing joli et rapide à mettre en place
+			// Chaque frame, on va déplacer la Caméra de <stiffness*100>% de la distance qui la sépare de la cible
+			// La formule est offset = (cible - valeur) * pourcentageDeProgression
+			// C'est équivalent à Mathf.Lerp(valeur, cible, pourcentageDeProgression);
+			// Pour une Vector3, C'est Vector3.Lerp(position, destination, pourcentageDeProgression)
+			moveOffset = (targetPos - new Vector3(this.transform.position.x, this.transform.position.y, 0)) * stiffness;
+		}
+
+		// Applique le déplacement à la position en additionnant les vecteurs
+		// équivalent de <code>this.transform.position = this.transform.position + moveOffset;</code>
+		this.transform.position += moveOffset;
+	}
+}

ce657c12f125ab9211ab8ffb340e1849b161420e
L4brax <ant.hess2@gmail.com>
2018-03-08 16:14:08 +0100

    Ajout du prefab item récuperable et du game manager

---
+++ Assets/antoineh/GameManagerMain.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerMain : MonoBehaviour {
+
+	public int fireDefense = 0;
+	public int fireAttack = 0;
+	public int frostDefense = 0;
+	public int frostAttack = 0;
+	
+}

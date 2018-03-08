e53bef10bc611f209ba412559ffda917843749c1
AlexLP <alexlepoupon@gmail.com>
2018-03-08 18:02:54 +0100

    Game V1, move find item and fight

---
+++ Assets/AlexandreLePoupon/Script/Finish.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Finish : MonoBehaviour {
+
+	private bool fightStarted;
+
+	public Transform player;
+
+	public Canvas cadreText;
+
+	public Text endText;
+
+	private List<string> text;
+
+	private int inc;
+
+	private bool dead;
+
+	private GameObject myCam;
+
+	private GameObject myPlayer;
+
+	private Tracking trackingScript;
+
+	private MovePlayer move;
+
+	private GameObject gameManager;
+	
+	private GameManagerMain stats;
+
+	// Use this for initialization
+	void Start () {
+		fightStarted = false;
+		dead = false;
+		text = new List<string>();
+
+		myCam = GameObject.FindWithTag("MainCamera");
+		trackingScript = (Tracking) myCam.GetComponent(typeof(Tracking));
+
+		
+		myPlayer = GameObject.FindWithTag("Player");
+		move = (MovePlayer) myPlayer.GetComponent(typeof(MovePlayer));
+
+		gameManager = GameObject.FindWithTag("GameManager");
+		stats = (GameManagerMain) gameManager.GetComponent(typeof(GameManagerMain));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(fightStarted==false && isFinalMap()) {
+			fightStarted = true;
+			inc = 0;
+			fight();
+		}
+		if(isFinalMap() && (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))) {
+			inc += 1;
+            endText.text = text[inc];
+		}
+	}
+
+	bool isFinalMap() {
+		int x = trackingScript.matriceEnCoursX;
+		int y = trackingScript.matriceEnCoursY;
+		if(x==1 && y==1) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	void fight() {
+		// Debug.Log("start fight");
+		// Debug.Log(player.position);
+
+		//Stop character
+		// Debug.Log(move.canMove);
+		move.canMove = false;
+
+		//Teleport character to the center of the map
+		GameObject map = trackingScript.mapEnCour();
+		float x = map.transform.position.x;
+		float y = map.transform.position.y;
+		player.position = new Vector3(x, y, 0);
+
+		//Display text
+		text.Add("A dragon is attacking you! You can not run away, you'll have to fight!");
+
+
+		text.Add("The dragon spits fire on you.");
+		if(stats.fireDefense > 0) {
+			text.Add("You protect yourself with your shield of fire.");
+		} else if(stats.frostDefense > 0) {
+			text.Add("You did not succeed in defending yourself. Your shield of ice melts following the breath of the dragon. You burn and die in horrible sulfur.");
+			dead = true;
+		} else {
+			text.Add("You have no shield to protect you from the dragon's breath! You burn and die in horrible sulfur.");
+			dead = true;
+		}
+
+		if(!dead) {
+			text.Add("It's your turn to hit!");
+		}
+
+		if(!dead && stats.frostAttack > 0) {
+			text.Add("Your icy sword-blow defeats the dragon!");
+		} else if(!dead && stats.fireAttack > 0) {
+			text.Add("Your flaming sword is ineffective against the dragon! He devours you ...");
+			dead = true;
+		} else if(!dead) {
+			text.Add("You don't have a sword to fight the dragon. He devours you ...");
+			dead = true;
+		}
+
+		if(!dead) {
+			text.Add("You win this fight, well play!");
+		} else {
+			text.Add("You loose for this time... Try again!");
+		}
+
+		endText.text = text[inc];
+		cadreText.sortingOrder = 3;
+
+	}
+}
---
+++ Assets/AlexandreLePoupon/Script/UserItem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserItem : MonoBehaviour {
+	public GameObject gameManager;
+	/**
+		Element of the object : Can be 'Fire' or 'Forst'
+	 */
+	public string element;
+	/**
+		Type of the object : Can be 'Defense' or 'Attack'
+	 */
+	public string type;
+
+	private void OnTriggerEnter2D () {
+		Debug.Log("Item picked !");
+		if (element == "Fire"){
+			if (type == "Defense"){
+				gameManager.GetComponent<GameManagerMain>().fireDefense += 10;
+			} else {
+				gameManager.GetComponent<GameManagerMain>().fireAttack += 10;
+			}
+		} else {
+			if (type == "Defense"){
+				gameManager.GetComponent<GameManagerMain>().frostDefense += 10;
+			} else {
+				gameManager.GetComponent<GameManagerMain>().frostAttack += 10;
+			}
+		}
+		Destroy(gameObject);
+	}
+}

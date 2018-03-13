a5421b490092f471e1e461623a45df454990ca7b
nirv44 <nicolas44.vivion@gmail.com>
2018-03-13 15:23:00 +0100

    news scene with new cards

--- Assets/Nicolas VIVION/Tracking.cs
+++ Assets/Nicolas VIVION/Tracking.cs
@@ -32,15 +32,28 @@ namespace vivion {
 		public int matriceEnCoursX = 0;
 		public int matriceEnCoursY = 0;
 
+		public float decalx = 0.18f;
+		public float decaly = 0.25f;
+
 		void Start () {
 			prepareMatrice();	
 			deplaceCamera();
 		}
 
-		
+		public float persoY = 0;
+		public float persoX = 0;
+		public float mapx = 0;
+		public float mapy = 0;
 		void Update () {
 
+			persoY = personnage.position.y;
+			persoX = personnage.position.x;
+
 			if(matrice.ContainsKey(getPositionMatrice())){
+
+				mapx =  matrice[getPositionMatrice()].transform.position.x;
+				mapy =  matrice[getPositionMatrice()].transform.position.y;
+
 				if(personnage.position.y < matrice[getPositionMatrice()].transform.position.y - (tailleMapY/2)){
 					changePositionMatrice(0,-1);
 				}
@@ -61,8 +74,8 @@ namespace vivion {
 
 		public void goTracking(Transform MyMaps){
 			Vector3 targetPos = new Vector3 (
-				MyMaps.position.x,
-				MyMaps.position.y,
+				(MyMaps.position.x-decalx),
+				(MyMaps.position.y-decaly),
 				this.transform.position.z
 			);
 			Vector3 moveOffset = Vector3.zero;
@@ -74,10 +87,38 @@ namespace vivion {
 
 
 		public void prepareMatrice(){
+			// matrice.Add("0_0", grid.transform.GetChild(0).gameObject);
+			// matrice.Add("1_0", grid.transform.GetChild(1).gameObject);
+			// matrice.Add("0_1", grid.transform.GetChild(2).gameObject);
+			// matrice.Add("1_1", grid.transform.GetChild(3).gameObject);
+
+			
 			matrice.Add("0_0", grid.transform.GetChild(0).gameObject);
 			matrice.Add("1_0", grid.transform.GetChild(1).gameObject);
-			matrice.Add("0_1", grid.transform.GetChild(2).gameObject);
-			matrice.Add("1_1", grid.transform.GetChild(3).gameObject);
+			matrice.Add("2_0", grid.transform.GetChild(2).gameObject);
+			matrice.Add("3_0", grid.transform.GetChild(3).gameObject);
+			matrice.Add("4_0", grid.transform.GetChild(4).gameObject);
+
+			
+			matrice.Add("0_1", grid.transform.GetChild(5).gameObject);
+			matrice.Add("1_1", grid.transform.GetChild(6).gameObject);
+			matrice.Add("2_1", grid.transform.GetChild(7).gameObject);
+			matrice.Add("3_1", grid.transform.GetChild(8).gameObject);
+			matrice.Add("4_1", grid.transform.GetChild(9).gameObject);
+			
+			matrice.Add("0_2", grid.transform.GetChild(10).gameObject);
+			matrice.Add("1_2", grid.transform.GetChild(11).gameObject);
+			matrice.Add("2_2", grid.transform.GetChild(12).gameObject);
+			matrice.Add("3_2", grid.transform.GetChild(13).gameObject);
+			matrice.Add("4_2", grid.transform.GetChild(14).gameObject);
+
+			matrice.Add("0_3", grid.transform.GetChild(15).gameObject);
+			matrice.Add("1_3", grid.transform.GetChild(16).gameObject);
+			matrice.Add("2_3", grid.transform.GetChild(17).gameObject);
+			matrice.Add("3_3", grid.transform.GetChild(18).gameObject);
+			matrice.Add("4_3", grid.transform.GetChild(19).gameObject);
+
+
 		}
 
 		public string getPositionMatrice(){
@@ -90,8 +131,8 @@ namespace vivion {
 		
 		public void deplaceCamera(){
 			this.transform.position = new Vector3(
-				matrice[getPositionMatrice()].transform.position.x,
-				matrice[getPositionMatrice()].transform.position.y,
+				(matrice[getPositionMatrice()].transform.position.x-decalx),
+				(matrice[getPositionMatrice()].transform.position.y-decaly),
 				this.transform.position.z
 			);
 		}
